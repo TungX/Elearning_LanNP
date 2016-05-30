084ede35b677c6605769b4ed74476ab64cfe7b42
Yin Long <blue.rose.hut@gmail.com>
2016-05-30 11:32:55 +0700

    add documnet

--- elearning_lannp.sql
+++ elearning_lannp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2016 at 12:56 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: May 30, 2016 at 06:13 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `elearning_lannp`
@@ -26,25 +26,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `assignments`
 --
 
-CREATE TABLE IF NOT EXISTS `assignments` (
+CREATE TABLE `assignments` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `type` int(1) NOT NULL,
-  `deadline` date NOT NULL,
   `course_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `assignment_has_attach_file`
---
-
-CREATE TABLE IF NOT EXISTS `assignment_has_attach_file` (
-  `id` int(11) NOT NULL,
-  `assignment_id` int(11) NOT NULL,
-  `attacj_file_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -53,13 +39,45 @@ CREATE TABLE IF NOT EXISTS `assignment_has_attach_file` (
 -- Table structure for table `attach_files`
 --
 
-CREATE TABLE IF NOT EXISTS `attach_files` (
+CREATE TABLE `attach_files` (
   `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `path` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `path` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL,
-  `time` datetime NOT NULL
+  `lecture_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `attach_files`
+--
+
+INSERT INTO `attach_files` (`id`, `name`, `path`, `user_id`, `lecture_id`) VALUES
+(2, 'Bài 2', 'http://mp3.zing.vn/playlist/Nhac-khong-loi-pigbeautifull/IOWZZFA7.html?st=7', 1, 13),
+(3, 'Bài 1', '', 1, 13),
+(4, 'Bài 3', 'http://mp3.zing.vn/playlist/Nhac-khong-loi-pigbeautifull/IOWZZFA7.html?st=31', 1, 13),
+(5, 'Bài 4', 'http://mp3.zing.vn/playlist/Nhac-khong-loi-pigbeautifull/IOWZZFA7.html?st=31', 1, 13),
+(6, 'Bài 5', 'https://www.facebook.com/', 1, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Công nghệ phần mềm'),
+(3, 'Hệ thống thông tin'),
+(2, 'Khoa học máy tính'),
+(4, 'Trí tuệ nhân tạo');
 
 -- --------------------------------------------------------
 
@@ -67,15 +85,23 @@ CREATE TABLE IF NOT EXISTS `attach_files` (
 -- Table structure for table `courses`
 --
 
-CREATE TABLE IF NOT EXISTS `courses` (
+CREATE TABLE `courses` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL,
   `password` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
-  `homework` text COLLATE utf8_unicode_ci NOT NULL,
-  `course_id` int(11) NOT NULL
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `name`, `description`, `user_id`, `password`, `category_id`) VALUES
+(1, 'Nhập môn công nghệ phần mềm', 'Môn bắt buộc đối với sinh viên công nghệ phần mềmm', 1, '', 1),
+(2, 'Hệ cơ sở tri thức', 'Môn học hay', 1, '', 2),
+(3, 'Xây dựng phần mềm\r\n', 'Hướng dẫn các bước xây dựng phần mềm', 1, '1', 1);
 
 -- --------------------------------------------------------
 
@@ -83,24 +109,24 @@ CREATE TABLE IF NOT EXISTS `courses` (
 -- Table structure for table `lectures`
 --
 
-CREATE TABLE IF NOT EXISTS `lectures` (
+CREATE TABLE `lectures` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(12) COLLATE utf8_unicode_ci NOT NULL
+  `password` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `course_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `lecture_has_attach_file`
+-- Dumping data for table `lectures`
 --
 
-CREATE TABLE IF NOT EXISTS `lecture_has_attach_file` (
-  `id` int(11) NOT NULL,
-  `lecture_id` int(11) NOT NULL,
-  `attach_file_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+INSERT INTO `lectures` (`id`, `name`, `description`, `password`, `course_id`) VALUES
+(9, 'Tuần 5', '', '', 0),
+(10, 'Tuần 6', '', '', 0),
+(11, 'Tuần 5', '', '', 0),
+(13, 'Giới thiệu', 'Giới thiệu công nghệ phần mềm', '', 1),
+(14, 'Các bước xây dựng phần mềm', 'Giới thiệu về các bước xây dưng phần mềm.', '', 1);
 
 -- --------------------------------------------------------
 
@@ -108,20 +134,22 @@ CREATE TABLE IF NOT EXISTS `lecture_has_attach_file` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `encrypted_password` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `first_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `last_name` varchar(10) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `last_name` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'uploads/avatar-default.jpg',
+  `type` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `encrypted_password`, `first_name`, `last_name`) VALUES
-(1, 'tungxbk@gmail.com', '123456', 'Yin', 'Long');
+INSERT INTO `users` (`id`, `email`, `encrypted_password`, `first_name`, `last_name`, `avatar`, `type`) VALUES
+(1, 'tungxbk@gmail.com', '123456', 'Yin', 'Long', 'uploads/avatar-default.jpg', 0);
 
 --
 -- Indexes for dumped tables
@@ -131,43 +159,45 @@ INSERT INTO `users` (`id`, `email`, `encrypted_password`, `first_name`, `last_na
 -- Indexes for table `assignments`
 --
 ALTER TABLE `assignments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `assignment_has_attach_file`
---
-ALTER TABLE `assignment_has_attach_file`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id_idx` (`course_id`);
 
 --
 -- Indexes for table `attach_files`
 --
 ALTER TABLE `attach_files`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id_idx` (`user_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name_UNIQUE` (`name`);
 
 --
 -- Indexes for table `courses`
 --
 ALTER TABLE `courses`
-  ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id_idx` (`category_id`),
+  ADD KEY `user_id_idx` (`user_id`);
 
 --
 -- Indexes for table `lectures`
 --
 ALTER TABLE `lectures`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `lecture_has_attach_file`
---
-ALTER TABLE `lecture_has_attach_file`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name_index` (`name`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`), ADD KEY `email` (`email`,`encrypted_password`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD KEY `email` (`email`,`encrypted_password`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -177,37 +207,54 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `assignment_has_attach_file`
---
-ALTER TABLE `assignment_has_attach_file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `attach_files`
 --
 ALTER TABLE `attach_files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `lectures`
 --
 ALTER TABLE `lectures`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `lecture_has_attach_file`
---
-ALTER TABLE `lecture_has_attach_file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD CONSTRAINT `course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `attach_files`
+--
+ALTER TABLE `attach_files`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
